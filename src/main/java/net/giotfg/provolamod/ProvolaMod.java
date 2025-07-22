fa0e7fe37feda5e5b4f218da22c947fa99b79f31
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-07-22 21:44:37 +0200

    Initial commit

---
+++ src/main/java/net/giotfg/provolamod/ProvolaMod.java
@@ -0,0 +1,16 @@
+package net.giotfg.provolamod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProvolaMod implements ModInitializer {
+	public static final String MOD_ID = "provola_mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/giotfg/provolamod/ProvolaModClient.java
@@ -0,0 +1,10 @@
+package net.giotfg.provolamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProvolaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
