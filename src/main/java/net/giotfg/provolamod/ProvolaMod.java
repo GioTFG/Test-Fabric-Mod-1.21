deebd9b6db398c5f711fee123cd846278c291a2e
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-07-23 11:48:42 +0200

    Aggiunti due oggetti senza utilità

--- src/main/java/net/giotfg/provolamod/ProvolaMod.java
+++ src/main/java/net/giotfg/provolamod/ProvolaMod.java
@@ -2,6 +2,7 @@ package net.giotfg.provolamod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.giotfg.provolamod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class ProvolaMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/giotfg/provolamod/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.giotfg.provolamod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.giotfg.provolamod.ProvolaMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+//    Si registrano tutti gli oggetti, salvati come costanti.
+    public static final Item GINO = registerItem("gino", new Item(new Item.Settings()));
+    public static final Item GINO_FOCOSO = registerItem("gino_focoso", new Item(new Item.Settings()));
+
+//    Metodo helepr per registrare il singolo oggetto. 'name' sarà l'id, quindi tutto minuscolo e solo underscore
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(ProvolaMod.MOD_ID, name), item);
+    }
+
+//    Metodo invocato all'inizializzazione della mod per registrare tutti gli oggetti
+    public static void registerModItems() {
+        ProvolaMod.LOGGER.info("Registering mod items for " + ProvolaMod.MOD_ID);
+
+//        Per aggiungere oggetti ad un determinato gruppo di oggetti.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(GINO);
+            entries.add(GINO_FOCOSO);
+        });
+    }
+}
