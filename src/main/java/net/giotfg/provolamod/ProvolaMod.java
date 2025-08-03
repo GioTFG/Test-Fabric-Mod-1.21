0269b1cc7c4bf91bcf6582c47e2242c92a087ddf
GioTFG <168219065+GioTFG@users.noreply.github.com>
2025-08-03 21:27:52 +0200

    Creato un nuovo evento

--- src/main/java/net/giotfg/provolamod/ProvolaMod.java
+++ src/main/java/net/giotfg/provolamod/ProvolaMod.java
@@ -2,6 +2,7 @@ package net.giotfg.provolamod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.giotfg.provolamod.block.ModBlocks;
@@ -9,6 +10,12 @@ import net.giotfg.provolamod.component.ModDataComponentTypes;
 import net.giotfg.provolamod.item.ModItemGroups;
 import net.giotfg.provolamod.item.ModItems;
 import net.giotfg.provolamod.util.HammerUsageEvent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.passive.SheepEntity;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,5 +35,18 @@ public class ProvolaMod implements ModInitializer {
 		FuelRegistry.INSTANCE.add(ModBlocks.FIRE_GINO_BLOCK, 20000);
 
 		PlayerBlockBreakEvents.BEFORE.register(new HammerUsageEvent());
+
+		AttackEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> {
+
+			if (entity instanceof SheepEntity sheepEntity && !world.isClient()) {
+				if (player.getMainHandStack().getItem() == Items.END_ROD) {
+					player.sendMessage(Text.literal("Just no..."));
+					player.getMainHandStack().decrement(1);
+					sheepEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, 5, 50));
+				}
+			}
+
+			return ActionResult.PASS;
+		}));
 	}
 }
